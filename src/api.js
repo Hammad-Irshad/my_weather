4bda3893a9b1056744a05eb2496157f768a650be
Hammad-Irshad <hammadirsad19@gmail.com>
2023-03-17 21:08:56 +0500

    final commit

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'b2f1c65b6dmsha9ba2116bb0f29ap180815jsnd7d9417231cf',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL ="https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL ="https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY ="44c4322cc9d510b7d2b7f4e0c90718e3";
